748ba46a3b6ed3d340f42ea2e2269b2d417d5b54
Mauricio <mutila@gmail.com>
2025-11-02 23:44:09 -0300

    teste de novas metricas

--- api/metrics.go
+++ api/metrics.go
@@ -2,10 +2,13 @@ package api
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 var (
@@ -20,8 +23,65 @@ var (
 	}, metricLabels)
 )
 
+// --- Métricas da aplicação (CPU, memória, rede) ---
+var (
+	procCPU = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "app_cpu_percent",
+		Help: "CPU usage percent of this application",
+	})
+	procMem = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "app_memory_bytes",
+		Help: "Memory usage of this application in bytes",
+	})
+	procNetSent = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "app_network_sent_bytes",
+		Help: "Network bytes sent by this application",
+	})
+	procNetRecv = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "app_network_recv_bytes",
+		Help: "Network bytes received by this application",
+	})
+)
+
 func registerMetric(e, m string, s int, i int64) {
 	c := fmt.Sprintf("%d", s)
 	requestCount.WithLabelValues(m, c, e).Inc()
 	requestDuration.WithLabelValues(m, c, e).Observe(float64(time.Now().UnixMilli() - i))
+}
+
+func collectAppMetrics() {
+	// Memória do Go
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	procMem.Set(float64(m.Alloc))
+
+	// CPU e rede do processo
+	pid := int32(os.Getpid())
+	proc, err := process.NewProcess(pid)
+	if err != nil {
+		return
+	}
+
+	// CPU %
+	cpuPercent, err := proc.CPUPercent()
+	if err == nil {
+		procCPU.Set(cpuPercent)
+	}
+
+	// Rede
+	ioCounters, err := proc.IOCounters()
+	if err == nil {
+		procNetSent.Set(float64(ioCounters.WriteBytes))
+		procNetRecv.Set(float64(ioCounters.ReadBytes))
+	}
+}
+
+// --- Inicia coleta automática ao importar o pacote ---
+func init() {
+	go func() {
+		for {
+			collectAppMetrics()
+			time.Sleep(5 * time.Second) // atualiza a cada 5 segundos
+		}
+	}()
 }
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/flatbuffers v25.9.23+incompatible // indirect
 	github.com/huandu/go-clone v1.7.3 // indirect
@@ -33,18 +34,25 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.18.1 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.67.2 // indirect
 	github.com/prometheus/procfs v0.19.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/otel v1.38.0 // indirect
 	go.opentelemetry.io/otel/metric v1.38.0 // indirect
